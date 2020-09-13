3e1923278153acdc2d77658019da35dcccff74d8
Farrid Kuntoro <farridkuntoro@mahasiswa.unikom.ac.id>
2020-09-13 14:34:08 +0700

    Adding basic placement body, footer, & sidebar 🤳

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer() {
     return (
         <div className="footer">
-            
+            <h1>This is footer</h1>
         </div>
     )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,7 +4,7 @@ import "./Sidebar.css";
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>This is sidebar</h1>
+            <img className="sidebar__logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
         </div>
     )
 }
