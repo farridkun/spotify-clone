b3e1a0f584d19b9a4b6b80e0669b8b51a02b748c
Farrid Kuntoro <farridkuntoro@mahasiswa.unikom.ac.id>
2020-09-13 10:56:12 +0700

    Login and add package spotify web api 👀

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(_token) {
+      setToken(_token)
+    }
+
+    console.log("I HAVE A TOKEN 💁‍♂️ ", token); 
+  }, []);
+
   return (
     <div className="app">
-      <h1>We bulding a Spotify Clone 🐱‍🚀</h1>
+      {
+        token ? <h1>I am logged in</h1> : <Login />
+      }
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,28 @@
+// https://developer.spotify.com/
+// documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = "dab17ce3e3234adb8b0f99ef24e2ef8b";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            // #accessToken=mysupersecretkeys&name=farrid
+            let parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1])
+            return initial;
+        }, {});
+}
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
